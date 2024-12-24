d00cdb4a8a93f5f2bb1bbf5b9f77c11dfe779d42
Shashank764 <154542556+Shashank764@users.noreply.github.com>
2024-12-25 03:41:08 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -6,7 +6,7 @@ public class calculator {
         Scanner sc=new Scanner(System.in);
         System.out.println("          CALCULATOR          ");
         System.out.println("------------------------------");
-        System.out.println("operations you can perform...!\nAddition:'+'\nSubtraction:'-'\nMultiplication:'*'\nDivision:'/'\nPower:'^'\nModulus'%'\nFactorial:'!'\nClear:'x'\n------------------------------");
+        System.out.println("operations you can perform...!\nAddition:'+'\nSubtraction:'-'\nMultiplication:'*'\nDivision:'/'\nPower:'^'\nModulus'%'\nFactorial:'!'\nClear:'C'\nExit:'X'\n------------------------------");
         String op;
         System.out.print("Enter the number: ");
         double a= sc.nextFloat();
@@ -22,66 +22,36 @@ public class calculator {
                     b= sc.nextFloat();
                     result+=b;
                     System.out.println("Answer: "+result);
-                    System.out.println("Want to continue...(Y/N)");
-                    String x= sc.next().toUpperCase();
-                    if(x.equals("N")){
-                        cont=false;
-                    }
                     break;
                 case "-":
                     System.out.print("Enter the number: ");
                     b= sc.nextFloat();
                     result-=b;
                     System.out.println("Answer: "+result);
-                    System.out.println("Want to continue...(Y/N)");
-                    String x1= sc.next().toUpperCase();
-                    if(x1.equals("N")) {
-                        cont = false;
-                    }
                     break;
                 case "*":
                     System.out.print("Enter the number: ");
                     b= sc.nextFloat();
                     result*=b;
                     System.out.println("Answer: "+result);
-                    System.out.println("Want to continue...(Y/N)");
-                    String x3= sc.next().toUpperCase();
-                    if(x3.equals("N")) {
-                        cont = false;
-                    }
                     break;
                 case "/":
                     System.out.print("Enter the number: ");
                     b= sc.nextFloat();
                     result/=b;
                     System.out.println("Answer: "+result);
-                    System.out.println("Want to continue...(Y/N)");
-                    String x4= sc.next().toUpperCase();
-                    if(x4.equals("N")){
-                        cont=false;
-                    }
                     break;
                 case "^":
                     System.out.print("Enter the number: ");
                     b= sc.nextFloat();
                     result=Math.pow(result,b);
-                    System.out.println("Answer: "+result);
-                    System.out.println("Want to continue...(Y/N)");
-                    String x5= sc.next().toUpperCase();
-                    if(x5.equals("N")) {
-                        cont = false;
-                    }
+                    System.out.println("Answer:- "+result);
                     break;
                 case "%":
                     System.out.print("Enter the number: ");
                     b= sc.nextFloat();
                     result%=b;
                     System.out.println("Answer: "+result);
-                    System.out.println("Want to continue...(Y/N)");
-                    String x6= sc.next().toUpperCase();
-                    if(x6.equals("N")) {
-                        cont = false;
-                    }
                     break;
                 case "!":
                     double f= result;
@@ -91,17 +61,15 @@ public class calculator {
                     }
                     result=fact;
                     System.out.println("Answer: "+(int)result);
-                    System.out.println("Want to continue...(Y/N)");
-                    String x7= sc.next().toUpperCase();
-                    if(x7.equals("N")) {
-                        cont = false;
-                    }
                     break;
-                case "X":
+                case "C":
                     result=0;
                     System.out.print("Enter the number: ");
                     double a2= sc.nextDouble();
                     result=a2;
+                    break;
+                case "X":
+                    cont=false;
                     break;
                 default:
                     System.out.println("Invalid Operation...!");
